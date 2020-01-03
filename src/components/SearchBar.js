79e726f7075529f72ae56d0a91c8bc68d5db02e7
tutkuince <tutku.ince@outlook.com>
2020-01-03 13:25:25 +0300

    updating state with fetched data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,7 +11,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (event) => {
         event.preventDefault();
 
-        // TODO: Make sure we call callback from parent component
+        this.props.onFormSubmit(this.state.term);
     };
 
     render() {
