3ab66cfc6c44fb2c7413af2f2e5620be58336775
tutkuince <tutku.ince@outlook.com>
2020-01-03 13:09:59 +0300

    Searching for videos with axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyB7QjHWU5RQ_zr8hNyEd-L25TdpXMsHx0E';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: `${KEY}`
+    }
+});
