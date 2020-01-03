4fb43c1c166bd1351ae23c20ec30cbebeca7895d
tutkuince <tutku.ince@outlook.com>
2020-01-03 13:50:07 +0300

    rendering video thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const VideoItem = () => {
-  return <div>VideoItem</div>;
+const VideoItem = ({video}) => {
+  return (
+      <div>
+        <img src={video.snippet.thumbnails.medium.url} alt=""/>
+      </div>
+  );
 };
 
 export default VideoItem;
